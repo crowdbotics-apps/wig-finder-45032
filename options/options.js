54c467e56d1ad16f39db13d24c35ac4e94f66c83
Crowdbotics <team@crowdbotics.com>
2023-12-15 17:44:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wig-finder-45032",
+  url: "https://wig-finder-45032.botics.co",
+  api: "https://wig-finder-45032.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
